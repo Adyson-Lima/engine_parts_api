025d47da455dd7573add6ba5219f65823e01ceb3
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-15 17:02:57 -0300

    17° commit - tdd controller: Consegue atualizar um part e retornar status 200? GREEN

--- app/controllers/api/v1/parts_controller.rb
+++ app/controllers/api/v1/parts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PartsController < ApplicationController
 
-  before_action :set_part, only: %i[show] # show update destroy
+  before_action :set_part, only: %i[show update] # show update destroy
 
   def index
     @parts = Part.all 
@@ -15,6 +15,14 @@ class Api::V1::PartsController < ApplicationController
     @part = Part.new(part_params)
     if @part.save
       render json: @part, status: :created, location: api_v1_part_url(@part)
+    else
+      render json: @part.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @part.update(part_params)
+      render json: @part
     else
       render json: @part.errors, status: :unprocessable_entity
     end
